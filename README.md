1a400cb5739af790905efcb164bc7b7b7c8eddde
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-08-10 03:43:27 -0700

    added elements, selectors, filled empty tags and starting my math.floor() function

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
--//- DAY ONE ,  
--- added wireframe / comments  to CSS & JavaScript
+# --// PROJECT DESCRIPTION.
+
+<center> 
+
+   (W . I . P .)   
+
+</center>
+
+
+## -//- DAY ZERO ,  
+>>
+-- re-created wireframe / added comments | for HTML & to JavaScript, finally added codes
+-- added color to page as a test
+
+   <center> 
+
+### --- resources and references; 
+ 
+   </center>
+
+#### topic -- math.floor();
+<b> Youtube </b> {} [x](https://www.youtube.com/watch?v=0n6hFpk62BI) <b> // </b>
+[x](https://www.youtube.com/watch?v=T9lTmitzzv8)
+
+
+## -//- DAY  ,  
+-- 
 -- 
 --
+
+
--- script.js
+++ script.js
@@ -1,23 +1,72 @@
-"use strict"
-//====//========||||||||||||||||||||||||||
-//====//========|| TESTING THE SANITY & HEADER TEXT
-alert("Sanity Test");
-const text = document.querySelector(".title");
-//====//========||||||||||||||||||||||||||
-//====//========|| create class base with properties
-class posterImages {
-//====//========|| /+/ attach the properties to the class
-//====//////////// calculate how many times the user clicked this option
-//====//////////// show how many times the option was presented and how many times it was clicked
-//clicks here
-//calculations here
-//====//========||||||||||||||||||||||||||
-//====//========||  a constructor
-constructor
-//====//========||||||||||||||||||||||||||
-//====//========|| begin picture list here 
-//====//////////// add funtion and object here
+'use strict'
+// begin sanity testing
+console.log('Sanity');
+//brief styling to body
+document.body.style.backgroundColor('salmon');
+// added title to page
+const text = document.querySelector('title'); // Header One
 
-//====//========||
-//====//========|| add functioning button here
-};
+// begin class & constructor / objects for fandom BusMall
+class fandomAlbum  {
+    clicks = 0;
+    timesShown =0;
+// added the constructor & targetted strings
+constructor(name, sub, imgSrc) {
+    this.name = name; // name of subject
+    this.sub = sub; // brief sub subject
+    this.imgSrc = imgSrc; // imagery of subject
+ };
+}   
+// add the variables & creating button functionality here
+let imagesOne = null;
+let imagesTwo = null;
+let allClicks = 0;
+const clickLimit = 3;
+
+/*
+let
+button.onclick = function(){  
+}
+*/
+
+// this function will generate random pictures from the object list
+// generate random images by utilizing the math.floor() function
+
+
+
+ // begin fandomObjects array begin
+let fandomObjects = [
+    new fandomAlbum('name','sub','./images/2.jpg'),
+    new fandomAlbum('name','sub','./images/3.png'), 
+    new fandomAlbum('name','sub','./images/4.png'),
+    new fandomAlbum('name','sub','./images/5.jpg'),
+/*    
+      new ('','','')
+      new ('','','') 
+      new ('','','')
+      new ('','','') 
+      new ('','','')
+      new ('','','') 
+//    new ('','','')
+//    new ('','','') 
+//    new ('','','')
+      new ('','','') 
+*/ 
+];
+// ref. to elements
+const fanHandler = document.getElementById('fanomHandler');
+const fanHeader = document.getElementById('fandomHeader');
+const fanFooter = document.getElementById('fandomFooter')
+const fanResult = document.getElementById('resultsArea');
+const fanAlbum = document.getElementById('fandomAlb');
+const fanPicOne = document.getElementById('pic-onefan');
+const fanNameOne = document.getElementById('name-onefan');
+const fanSubOne = document.getElementById('sub-onefan');
+const fanPicTwo = document.getElementById('pic-twofan');
+const fanNameTwo = document.getElementById('name-twofan');
+const fanSubTwo = document.getElementById('sub-twofan');
+const fanPicThree = document.getElementById('pic-threefan');
+const fanNameThree = document.getElementById('name-threefan');
+const fanSubTwoThree = document.getElementById('sub-threefan');
+
+let fanButton = document.getElementById('getResults');
