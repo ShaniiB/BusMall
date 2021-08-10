29ecea0c56a1958eff1e39329fc4489dda9048b1
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-08-10 15:46:12 -0700

    revamped, problem found

--- script.js
+++ script.js
@@ -1,114 +1,55 @@
-'use strict'
-// begin sanity testing
-console.log('Sanity');
-//brief styling to body
-const body = document.querySelector('body');
-body.style.backgroundColor = 'salmon';
-// added title to page
-const text = document.querySelector('title'); // Header One
+// do the sanity test.
+console.log('sanity');
 
-// begin class & constructor / objects for fandom BusMall
-class fandomAlbum  {
+// the start of the class, holder of my objects that will be applied to my click-y image-y - beginning to get data worth logging.
+class fandomAlbum {
     clicks = 0;
-    timesShown =0;
-// added the constructor & targetted strings
-constructor(name, sub, imgSrc) {
-    this.name = name; // name of subject
-    this.sub = sub; // brief sub subject
-    this.imgSrc = imgSrc; // imagery of subject
+    optionShown = 0;
+    constructor(name, sub, imgSrc) {
+       this.name = name;
+       this.sub = sub;
+       this.imgSrc = imgSrc;
  }
-}   
-// add the variables & creating button functionality here
-let clickdOne = null;
-let clickdTwo = null;
-let clickdThree = null;
-let allClicks = 0;
-const clickLimit = 8;
-
-/*
-let button.onclick = function(){  
 }
-*/
-
- // begin fandomObjects array list
+// there should be 20 pictures inside of this object.
 let fandomObjects = [
-    new fandomAlbum('name','sub',''),
-    new fandomAlbum('name','sub','./images/3.png'), 
-    new fandomAlbum('name','sub','./images/4.png'),
-    new fandomAlbum('name','sub','./images/5.jpg')
-/*    
-      new ('','','')
-      new ('','','') 
-      new ('','','')
-      new ('','','') 
-      new ('','','')
-      new ('','','') 
-//    new ('','','')
-//    new ('','','') 
-//    new ('','','')
-      new ('','','') 
-*/ 
+   new fandomAlbum('Uzu','MHA','media/images/2.jpg'),
+   new fandomAlbum('Maki','MHA','media/images/3.png'),
+   new fandomAlbum('Maki','MHA','media/images/4.png'),
+   new fandomAlbum('Uzu', 'MHA', 'media/images/5.jpg')
 ];
-console.log(fandomObjects);
-// ref. to elements
-const fanHandler = document.getElementById('fandomHandler');
-const fanHeader = document.getElementById('fandomHeader');
-const fanFooter = document.getElementById('fandomFooter')
-const fanResult = document.getElementById('resultsArea');
-const fanAlbum = document.getElementById('fandomAlb');
 
-let fanPicOne = document.getElementById('pic-onefan');
-let fanNameOne = document.getElementById('name-onefan');
-let fanSubOne = document.getElementById('sub-onefan');
-let fanPicTwo = document.getElementById('pic-twofan');
-let fanNameTwo = document.getElementById('name-twofan');
-let fanSubTwo = document.getElementById('sub-twofan');
-let fanPicThree = document.getElementById('pic-threefan');
-let fanNameThree = document.getElementById('name-threefan');
-let fanSubThree = document.getElementById('sub-threefan');
+// using DOM by creating the 4 holders-of-the-apoc
+// set one
+   let nameOne = document.getElementsByClassName('name-one');
+   let picOne = document.getElementById('pic-one');
+   let subOne = document.getElementById('sub-one');
+// set two
+   let nameTwo = document.getElementsByClassName('name-two');
+   let picTwo = document.getElementById('pic-two');
+   let subTwo = document.getElementById('sub-two');
+// set three
+   let nameThree = document.getElementsByClassName('name-three');
+   let picThree = document.getElementById('pic-three');
+   let subThree = document.getElementById('sub-three');
+// set four
+   let nameFour = document.getElementsByClassName('name-four');
+   let picFour = document.getElementById('pic-four');
+   let subFour = document.getElementById('sub-four');
 
-let fanButton = document.getElementById('getResults');
-
-// array loop functionality
-//for (let otaku = 0; otaku < fandomObjects.length; otaku++ ); {
-//    console.log(fandomObjects);
-
-
-// this function will generate random pictures from the object list
-// generate random images by utilizing the math.floor() function
-
-let anoFandom = function() { // pick from the 4 images
-    
-let oneOtaku = Math.floor(Math.random()* fandomObjects.length);
-let twoOtaku = Math.floor(Math.random()* fandomObjects.length);
-let threeOtaku = Math.floor(Math.random()* fandomObjects.length);
-
-// random pick number 1 from fandomObjects
-    fanNameOne = fandomObjects[oneOtaku].name;
-    fanSubOne = fandomObjects[oneOtaku].sub;  
-    fanPicOne = fandomObjects[oneOtaku].imgSrc;
-    clickdOne = fandomObjects[oneOtaku];
-
-// random pick number 2 from fandomObjects
-    fanNameTwo = fandomObjects[twoOtaku].name;
-    fanSubTwo = fandomObjects[twoOtaku].sub;     
-    fanPicTwo = fandomObjects[twoOtaku].imgSrc;
-    clickdTwo = fandomObjects[twoOtaku];
-
-// random pick number 3 from fandomObjects
-    fanNameThree = fandomObjects[threeOtaku].name;
-    fanSubThree = fandomObjects[threeOtaku].sub;     
-    fanPicThree = fandomObjects[threeOtaku].imgSrc;
-    clickdThree = fandomObjects[threeOtaku];
-// random pick number 3 from fandomObjects
-
-// 
-};
-// calculating the clicks for the final results
-// event onClicks
-function clickAgeddon(event) {
-let allClicks = EventTarget;
-
-} 
-
-anoFandom();
+// give a value to my Objects using a function.  
+for (let otaku = 0; otaku < fandomObjects.length; otaku++) {
+    console.log(fandomObjects[0]);
+}   
+// set one
+picOne.src = fandomObjects[0].imgSrc;
+subOne.innerText = fandomObjects[0].name;
+// set two
+picTwo.src = fandomObjects[1].imgSrc;
+subTwo.innerText = fandomObjects[1].name;
+// set three
+picThree.src = fandomObjects[2].imgSrc;
+subThree.innerText = fandomObjects[2].name;
+// set four
+picFour.src = fandomObjects[3].imgSrc;
+subFour.innerText = fandomObjects[3].name;
